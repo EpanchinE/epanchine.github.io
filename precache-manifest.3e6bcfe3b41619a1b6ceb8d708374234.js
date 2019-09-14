48306013c0575258613abbf22752eebe26f67623
EpanchinE <epanchine@gmail.com>
2019-09-14 16:27:45 +0300

    Updates

---
+++ precache-manifest.3e6bcfe3b41619a1b6ceb8d708374234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b052c3f715e9c22f3671e23ef37a87",
+    "url": "/kbd/index.html"
+  },
+  {
+    "revision": "9cfca4fa7d351109a0a4",
+    "url": "/kbd/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e30ddcbdfe73c6b60712",
+    "url": "/kbd/static/js/2.80400384.chunk.js"
+  },
+  {
+    "revision": "9cfca4fa7d351109a0a4",
+    "url": "/kbd/static/js/main.a21726c7.chunk.js"
+  },
+  {
+    "revision": "3e39d3343e31b1117cda",
+    "url": "/kbd/static/js/runtime~main.09d82ab1.js"
+  }
+]);
